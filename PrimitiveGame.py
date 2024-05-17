d06e00fb54a708a51bc3fc154cb9ede8774d3071
txshfiq <110201236+txshfiq@users.noreply.github.com>
2024-05-16 20:16:47 -0400

    Cleaning up PrimitiveGame.py - 2

--- PrimitiveGame.py
+++ PrimitiveGame.py
@@ -3,13 +3,14 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+# mag(arr) computes the magnitude of a vector 'arr':
 def mag(arr):
     total = 0
     for d in arr:
         total += d**2
     return np.sqrt(total)
 
-# WINDOW INITIALIZATIONS
+# WINDOW INITIALIZATION
 pygame.init()
 width = 1000
 height = 600
@@ -36,9 +37,10 @@ ensemble = []
 speed_dist = []
 centers = []
 
-# Prepares particle ensemble 
+# prepares particles: 
 for _ in range(num_balls):
     b = False
+    # ensures that particle locations are randomized and do not overlap 
     while not b:
         center = [random.randint(20, width-20), random.randint(20, height-20)]
         b = True
@@ -55,17 +57,19 @@ for _ in range(num_balls):
                 break
     centers.append(center)
     ball = pygame.draw.circle(screen, black, center, r)
+
+    # initializing particle velocities
     v_x = random.uniform(3, 3)
     v_y = random.uniform(3, 3)
     if v_x == 0:
         v_x = random.randint(-1, 1)
     if v_y == 0:
         v_y = random.randint(-1, 1)
-    vel = [v_x, v_y]  # Random speed for each ball
-    ensemble.append((ball, vel))
+    vel = [v_x, v_y]  # insert velocity components into velocity vector
+    ensemble.append((ball, vel)) # create particle ensemble
         
-# Main frame flipper
-while not any(pygame.key.get_pressed()):
+# frame updater
+while not any(pygame.key.get_pressed()): # simulation stops when any key is pressed
     
     # INITIALIZATIONS
     vel_arr = []
@@ -73,21 +77,19 @@ while not any(pygame.key.get_pressed()):
     s = []
     speed_sq = 0
     
-    screen.fill(white)  # Maintain white background
+    screen.fill(white)  # maintain white background
 
     for i, (ball, vel) in enumerate(ensemble):
         vel_arr.append(vel)
-        ball = ball.move(vel)
+        ball = ball.move(vel) # move particle per frame
 
-        # if ball goes out of screen then change direction of movement
+        # if ball goes out of screen then change direction of movement:
         if ball.left < 0:
             ball.left = 0
             vel[0] = -vel[0]
         elif ball.right > width:
             ball.right = width
             vel[0] = -vel[0]
-
-        # Check vertical boundaries
         if ball.top < 0:
             ball.top = 0
             vel[1] = -vel[1]
@@ -95,68 +97,53 @@ while not any(pygame.key.get_pressed()):
             ball.bottom = height
             vel[1] = -vel[1]
             
-        k = 0
         for j, (other_ball, other_vel) in enumerate(ensemble[i + 1:], start=i + 1):
             dx = other_ball.centerx - ball.centerx
             dy = other_ball.centery - ball.centery
             distance = (dx ** 2 + dy ** 2) ** 0.5
-            if distance < (r * 2):  # If distance is less than sum of radii, they've collided
-                # Update velocities
+            if distance < (r * 2):  # if distance between two particles is less than sum of radii, they've collided
+                
+                # renaming variables:
                 x_pos, y_pos = ball.centerx, ball.centery
                 other_x_pos, other_y_pos = other_ball.centerx, other_ball.centery
                 x_1 = np.array([x_pos, y_pos])
                 x_2 = np.array([other_x_pos, other_y_pos])
-
                 v_1 = np.array(vel)
                 v_2 = np.array(other_vel)
 
+                # updating velocities: 
                 vel_1_f = v_1 - (((v_1 - v_2) @ ((x_1 - x_2))/((mag((x_1 - x_2)))**2))*(x_1 - x_2))
                 vel_2_f = v_2 - (((v_2 - v_1) @ ((x_2 - x_1))/((mag((x_2 - x_1)))**2))*(x_2 - x_1))
+                
+                # preventing division of 0 in velocity update (TO BE FIXED IN THE FUTURE): 
                 if mag((x_1 - x_2)) != 0:
                     vel[0], other_vel[0] = vel_1_f[0], vel_2_f[0]
-                    vel[1], other_vel[1] = vel_1_f[1], vel_2_f[1]
-                else:
-                    k = k + 1
-                    print("WEIRD AHH COLLISION", x_1 - x_2, k)
-                
+                    vel[1], other_vel[1] = vel_1_f[1], vel_2_f[1]         
 
-                '''''
-                for i in range(2):
-                    if vel[i] != 0 or other_vel[i] != 0:
-                        if vel[i] > 0:
-                            vel[i] = vel[i] - 1
-                        if vel[i] < 0:
-                            vel[i] = vel[i] + 1
-                        if other_vel[i] > 0:
-                            other_vel[i] = other_vel[i] - 1
-                        if other_vel[i] < 0:
-                            other_vel[i] = other_vel[i] + 1
-                '''''
-                
-                
-
-        ensemble[i] = (ball, vel)  # Update ball position in the list
-        pygame.draw.circle(surface=screen, color=red, center=ball.center, radius=r)
+        ensemble[i] = (ball, vel)  # update particle location in the list
+        pygame.draw.circle(surface=screen, color=red, center=ball.center, radius=r) # draw particle
     
     for vel in vel_arr:
         speed = (vel[0] ** 2 + vel[1] ** 2) ** 0.5
         speed_sq += speed**2
         s.append(speed)
-    ke = (1/2)*mass*(speed_sq)                 # This computes the total energy of the system (in Joules)
-    avg_speed_sq = speed_sq/num_balls          # This computes the average squared speed of the system
+    ke = (1/2)*mass*(speed_sq)                 # this computes the total energy of the system (in Joules)
+    avg_speed_sq = speed_sq/num_balls          # this computes the average squared speed of the system per frame
     
     
-
+    # computing average speed of system per frame:
     speeds_arr = np.array(s)
     speed_dist = speeds_arr
     avg_speed = np.sum(speeds_arr) / num_balls
     data.append(avg_speed)
 
-    temp = (mass*np.pi*(avg_speed**2))/(2*boltz_k)    # This computes the temperature of the system
+    temp = (mass*np.pi*(avg_speed**2))/(2*boltz_k)    # this computes the temperature of the system per frame
 
+    # text rendering:
+    
     text_surface_1 = FONT.render(f"Average Speed: {round(avg_speed, 5)}", True, black)
     text_rect_1 = text_surface_1.get_rect()
-    text_rect_1.center = (150, height-30)
+    text_rect_1.center = (150, height-30)                                                       
     screen.blit(text_surface_1, text_rect_1)
 
     text_surface_2 = FONT.render(f"Total Energy (U): {round(avg_speed_sq, 5)} J", True, black)
@@ -168,12 +155,8 @@ while not any(pygame.key.get_pressed()):
     text_rect_3 = text_surface_3.get_rect()
     text_rect_3.center = (width-180, 30)
     screen.blit(text_surface_3, text_rect_3)                 
-
-    # update screen
+ 
     pygame.display.flip()
-
-    
-
     clock.tick(fps)
 
     for event in pygame.event.get():
@@ -181,19 +164,19 @@ while not any(pygame.key.get_pressed()):
         if event.type == pygame.QUIT:
             exit()
 
-
+# creates histogram displaying the distribution of particle velocities after simulation stops:
 plt.hist(speed_dist, bins=50, color='blue', alpha=0.7)
 plt.xlabel('Speed')
 plt.ylabel('Frequency')
 plt.title('Speed Distribution')
 plt.show()
 
-'''''
+# plots average speed as a function of time:
 plt.plot(data)
 plt.xlabel("Time")
 plt.ylabel("Average Speed")
 plt.title("Average Speed Over Time")
 plt.show()
-'''''
+
 
 
